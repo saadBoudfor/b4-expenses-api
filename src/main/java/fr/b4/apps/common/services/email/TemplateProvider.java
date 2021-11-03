3d7c0802a1ad46524418d971b6ec34f32130ddde
Saâd BOUDFOR <saad.boudfor.externe@odigo.com>
2021-11-03 15:17:42 +0100

    feature send daily report

--- src/main/java/fr/b4/apps/common/services/email/TemplateProvider.java
+++ src/main/java/fr/b4/apps/common/services/email/TemplateProvider.java
@@ -3,17 +3,15 @@ package fr.b4.apps.common.services.email;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
+import java.io.InputStream;
 
 public class TemplateProvider {
     public static String getReportMailTemplate(String name, String target, String total, String date) throws IOException {
 
         Resource resource = new ClassPathResource("static/report.html");
-        File file = resource.getFile();
-        String report = new String(
-                Files.readAllBytes(file.toPath()));
+        InputStream inputStream = resource.getInputStream();
+        String report = new String(inputStream.readAllBytes());
         return report.replace("name", name)
                 .replace("target", target)
                 .replace("total", total)
