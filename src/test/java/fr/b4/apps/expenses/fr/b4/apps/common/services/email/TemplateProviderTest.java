b87c252df2e0b2f3e999be510dcee7fce1225902
Saâd BOUDFOR <saad.boudfor.externe@odigo.com>
2021-11-03 20:18:49 +0100

    add tests

--- src/test/java/fr/b4/apps/expenses/fr/b4/apps/common/services/email/TemplateProviderTest.java
+++ src/test/java/fr/b4/apps/expenses/fr/b4/apps/common/services/email/TemplateProviderTest.java
@@ -2,6 +2,7 @@ package fr.b4.apps.expenses.fr.b4.apps.common.services.email;
 
 import fr.b4.apps.common.services.email.TemplateProvider;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -11,10 +12,13 @@ public class TemplateProviderTest {
 
 
     @Test
+    @DisplayName("test template generation")
     void shouldReturnMailTemplate() throws IOException {
-        String mail = TemplateProvider.getReportMailTemplate("saad", "32.32", "15.60", LocalDate.now().toString());
+        String today =  LocalDate.now().toString();
+        String mail = TemplateProvider.getReportMailTemplate("saad", "32.32", "15.60", today);
         Assertions.assertTrue(mail.contains("saad"));
         Assertions.assertTrue(mail.contains("32.32"));
         Assertions.assertTrue(mail.contains("15.60"));
+        Assertions.assertTrue(mail.contains(today));
     }
 }
