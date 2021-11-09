280ab690a2c3d83b53693d00bf1191b62c520f29
Saâd BOUDFOR <saad.boudfor.externe@odigo.com>
2021-11-09 11:37:28 +0100

    rework on stat webservice

--- src/main/java/fr/b4/apps/expenses/dto/ExpenseDTO.java
+++ src/main/java/fr/b4/apps/expenses/dto/ExpenseDTO.java
@@ -1,6 +1,10 @@
 package fr.b4.apps.expenses.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import fr.b4.apps.clients.entities.User;
 import fr.b4.apps.common.entities.Place;
 import lombok.Data;
@@ -14,6 +18,9 @@ public class ExpenseDTO {
     private String name;
     private List<ExpenseLineDTO> expenseLines;
     private String comment;
+
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate date;
     private User author;
