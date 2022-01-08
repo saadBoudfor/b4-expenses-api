e7b1d0ff5ffdc17550cb46b10a526ca0b958fe42
Saâd BOUDFOR <saad.boudfor.externe@odigo.com>
2022-01-08 18:40:55 +0100

    003: add bucketController (define bucket web services)

---
+++ src/main/java/fr/b4/apps/stores/services/BucketService.java
@@ -0,0 +1,29 @@
+package fr.b4.apps.stores.services;
+
+import fr.b4.apps.stores.dto.BucketDTO;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class BucketService {
+    public BucketDTO save(BucketDTO bucket) {
+        return null;
+    }
+
+    public BucketDTO update(BucketDTO bucket) {
+        return null;
+    }
+
+    public void delete(long bucketID) {
+
+    }
+
+    public List<BucketDTO> filterByStoreId(long id) {
+        return null;
+    }
+
+    public List<BucketDTO> filterByUserId(long id) {
+        return null;
+    }
+}
