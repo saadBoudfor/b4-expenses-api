513620805d6359971bf09947e0d36b2fb3af79b8
Saâd BOUDFOR <saad.boudfor.externe@odigo.com>
2022-01-09 17:04:50 +0100

    004: fix mvn job

--- src/test/java/fr/b4/apps/expenses/process/ItemProcessTests.java
+++
@@ -1,9 +0,0 @@
-package fr.b4.apps.expenses.process;
-
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
-
-@RunWith(MockitoJUnitRunner.class)
-public class ItemProcessTests {
-
-}
