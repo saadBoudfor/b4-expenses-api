a33edad95178cc7fa4d4b38d44afed5c6e9cf086
Saâd BOUDFOR <saad.boudfor.externe@odigo.com>
2022-01-09 17:03:47 +0100

    004: fix mvn job

--- src/main/java/fr/b4/apps/stores/process/ItemProcess.java
+++ src/main/java/fr/b4/apps/stores/process/ItemProcess.java
@@ -1,7 +1,9 @@
 package fr.b4.apps.stores.process;
 
 import fr.b4.apps.stores.dto.ItemDTO;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ItemProcess {
     public ItemDTO save(ItemDTO itemDTO) {
         return null;
