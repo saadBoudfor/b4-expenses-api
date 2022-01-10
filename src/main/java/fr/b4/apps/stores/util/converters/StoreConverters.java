54244075b89409cc66f8ad0e58c371ab3531b0ae
Saâd BOUDFOR <saad.boudfor.externe@odigo.com>
2022-01-10 10:53:43 +0100

    005: fix add item issue

--- src/main/java/fr/b4/apps/stores/util/converters/StoreConverters.java
+++ src/main/java/fr/b4/apps/stores/util/converters/StoreConverters.java
@@ -3,10 +3,14 @@ package fr.b4.apps.stores.util.converters;
 import fr.b4.apps.stores.dto.StoreDTO;
 import fr.b4.apps.stores.entities.Store;
 import lombok.experimental.UtilityClass;
+import org.apache.commons.lang3.ObjectUtils;
 
 @UtilityClass
 public class StoreConverters {
     public static Store toStore(StoreDTO dto) {
+        if(ObjectUtils.isEmpty(dto)) {
+            return null;
+        }
         Store store = new Store();
         store.setName(dto.getName());
         store.setOwner(dto.getOwner());
@@ -16,6 +20,9 @@ public class StoreConverters {
     }
 
     public static StoreDTO toDTO(Store store) {
+        if(ObjectUtils.isEmpty(store)) {
+            return null;
+        }
         StoreDTO dto = new StoreDTO();
         dto.setName(store.getName());
         dto.setOwner(store.getOwner());
